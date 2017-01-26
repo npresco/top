4db19593058ac34043bb2f9336188b799dae0895
Noah Prescott <noah.prescott@gmail.com>
2017-01-26 01:34:47 -0500

    Add knight and knight_spec

--- chess/knight.rb
+++ chess/knight.rb
@@ -10,5 +10,32 @@ class Knight < Piece
     end
   end
 
+  # Piece move
+  #
+  # @param {Array} - board
+  # @param {String} - position
+  # @return {Array}
+  def move(board, position)
+    position_row = position[0]
+    position_column = position[1].to_i
+    if (self.position_row.ord - position_row.ord).abs == 1 &&
+       (self.position_column - position_column).abs == 2 &&
+       !board[GRID[position]] ||
+       (self.position_row.ord - position_row.ord).abs == 2 &&
+       (self.position_column - position_column).abs == 1 &&
+       !board[GRID[position]]
+       super(board, position)
+    elsif (self.position_row.ord - position_row.ord).abs == 1 &&
+          (self.position_column - position_column).abs == 2 &&
+          board[GRID[position]].color != self.color ||
+          (self.position_row.ord - position_row.ord).abs == 2 &&
+          (self.position_column - position_column).abs == 1 &&
+          board[GRID[position]].color != self.color
+       super(board, position)
+     else
+       board
+     end
+  end
+
 
 end
--- chess/spec/knight_spec.rb
+++ chess/spec/knight_spec.rb
@@ -1,9 +1,33 @@
 require_relative "../chess.rb"
 
 RSpec.describe Knight do
+  let(:game) {Game.new}
+  let(:board) {game.empty_board}
+  let(:e4) {board[35] = Knight.new("white", "e4")}
+
   describe "#move" do
     it "moves 2 spaces in column and 1 in row" do
-      
+      e4.move(board, "g5")
+      expect(board[52].position).to eq("g5")
+    end
+
+    it "moves over pieces" do
+      board[43] = Pawn.new("white", "f4")
+      board[51] = Pawn.new("black", "g4")
+      e4.move(board, "g5")
+      expect(board[52].position).to eq("g5")
+    end
+
+    it "takes a piece of the opposite color" do
+      board[52] = Pawn.new("black", "g5")
+      e4.move(board, "g5")
+      expect(board[52].color).to eq("white")
+    end
+
+    it "doesn't take a piece of the same color" do
+      board[52] = Pawn.new("white", "g5")
+      e4.move(board, "g5")
+      expect(board[35].color).to eq("white")
     end
   end
 end
