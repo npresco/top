49f7243bbd824ff91f317f39f8a360e36d25eba1
Noah Prescott <noah.prescott@gmail.com>
2017-03-27 16:25:46 -0400

    Include ceaser_cipher method in sinatra file

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,33 @@
 require "sinatra"
-require_relative "../../ruby_building_blocks/caesar_cipher.rb"
 
 set :sessions, key:            "N&wedhSDF",
                expire_after:   14_400,
                secret:         "*&(^B234"
+
+def caesar_cipher(sentence, number=0)
+  upcases = ("A".."Z").to_a
+  downcases = ("a".."z").to_a
+
+  downhash = Hash[("a".."z").to_a.zip((0..25).to_a)]
+  uphash = Hash[("A".."Z").to_a.zip((0..25).to_a)]
+
+  letters = sentence.split("")
+
+  letters.map! do |x|
+    if downhash.include? x
+      x = downhash[x] + number
+      x -= 26 while x > 25
+      x = downcases[x]
+    elsif uphash.include? x
+      x = uphash[x] + number
+      x -= 26 while x > 25
+      x = upcases[x]
+    else
+      x
+    end
+  end
+  p letters.join
+end
 
 get "/" do
   erb :index, locals: { cipher: session[:cipher] }
